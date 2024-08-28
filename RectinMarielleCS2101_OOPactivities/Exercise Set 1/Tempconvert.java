81a74b84859554d7ea66e671a42126faef01110b
Marielle Rectin <157998090+onlymarkive@users.noreply.github.com>
2024-08-28 08:08:34 +0800

    Update Tempconvert.java

--- RectinMarielleCS2101_OOPactivities/Exercise Set 1/Tempconvert.java
+++ RectinMarielleCS2101_OOPactivities/Exercise Set 1/Tempconvert.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TemperatureConverter {
+public class Tempconvert {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
